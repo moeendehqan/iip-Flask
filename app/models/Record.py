34ea1a059bc842e0457ce0fb2f61dc8df1d87781
moeendehqan <moeen.dehqan@gmail.com>
2023-12-21 01:02:00 +0330

    .

--- app/models/Record.py
+++ app/models/Record.py
@@ -8,14 +8,17 @@ cl = db['Record']
 
 
 class Record():
-    def set_record(self,id_camera, ip, port, type, connect, error, image, plate, status, object):
+    def set_record(self, id_camera, ip, port, rule_type, connect, error, image, plate, status, obj):
         current_time = datetime.datetime.now()
         one_minute_ago = current_time - datetime.timedelta(seconds=20)
-        dic = {'id_comera':id_camera, 'connect':connect,' error':error, 'timestamp':time.time()*1000, "datetime":current_time, 'ip':ip, 'port':port, 'type':type, 'image':image, 'plate':plate, 'status':status, 'object':object}
-        cl.delete_many({'id_comera':id_camera, 'datetime': {'$lt': one_minute_ago}})
+        dic = {'id_camera': id_camera, 'connect': connect, 'error': error, 'timestamp': time.time() * 1000,
+               "datetime": current_time, 'ip': ip, 'port': port, 'typeRule': rule_type, 'image': image, 'plate': plate,
+               'status': status, 'object': obj}
+        cl.delete_many({'id_camera': id_camera, 'datetime': {'$lt': one_minute_ago}})
+
         return cl.insert_one(dic)
     
     def get_last_frame_record(self,id_camera):
-        res = cl.find_one({'id_comera':id_camera},sort=[('timestamp', -1)])
+        res = cl.find_one({'id_camera':id_camera},sort=[('timestamp', -1)])
         return res
 
