053d9f31471cb39c31a9aad2cd4860b18f7f3741
moeen dehqan <moeen.dehqan@gmail.com>
2023-12-20 14:22:39 +0330

    .

--- app/models/Record.py
+++ app/models/Record.py
@@ -8,12 +8,11 @@ cl = db['Record']
 
 
 class Record():
-    def set_record(self,id_camera, ip, port, type, connect, error, image, plate, status):
+    def set_record(self,id_camera, ip, port, type, connect, error, image, plate, status, object):
         current_time = datetime.datetime.now()
         one_minute_ago = current_time - datetime.timedelta(seconds=20)
-        dic = {'id_comera':id_camera, 'connect':connect,' error':error, 'timestamp':time.time()*1000, "datetime":current_time, 'ip':ip, 'port':port, 'type':type, 'image':image, 'plate':plate, 'status':status}
+        dic = {'id_comera':id_camera, 'connect':connect,' error':error, 'timestamp':time.time()*1000, "datetime":current_time, 'ip':ip, 'port':port, 'type':type, 'image':image, 'plate':plate, 'status':status, 'object':object}
         cl.delete_many({'id_comera':id_camera, 'datetime': {'$lt': one_minute_ago}})
-
         return cl.insert_one(dic)
     
     def get_last_frame_record(self,id_camera):
