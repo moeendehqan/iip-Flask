f7a4d7964033b5efa0e2b7c70fe3994f16a28a5b
moeen dehqan <moeen.dehqan@gmail.com>
2023-12-21 13:59:21 +0330

    .

--- app/models/Record.py
+++ app/models/Record.py
@@ -8,10 +8,10 @@ cl = db['Record']
 
 
 class Record():
-    def set_record(self,id_camera, ip, port, type, connect, error, image, plate, status, object):
+    def set_record(self,id_camera, ip, port, type, connect, error, image, plate, status):
         current_time = datetime.datetime.now()
         one_minute_ago = current_time - datetime.timedelta(seconds=20)
-        dic = {'id_comera':id_camera, 'connect':connect,' error':error, 'timestamp':time.time()*1000, "datetime":current_time, 'ip':ip, 'port':port, 'type':type, 'image':image, 'plate':plate, 'status':status, 'object':object}
+        dic = {'id_comera':id_camera, 'connect':connect,' error':error, 'timestamp':time.time()*1000, "datetime":current_time, 'ip':ip, 'port':port, 'type':type, 'image':image, 'plate':plate, 'status':status}
         cl.delete_many({'id_comera':id_camera, 'datetime': {'$lt': one_minute_ago}})
         return cl.insert_one(dic)
     
--- app/models/Rules.py
+++ app/models/Rules.py
@@ -10,11 +10,11 @@ cl = db['Rules']
 
 
 class Rulse():
-    def set_rules(self,creator, type, selectDevice, idplate, alpha, serial, city, status, pinout, duration):
+    def set_rules(self,creator, idplate, alpha, serial, city, status):
         duplicate = cl.find_one({'idplate':idplate,'alpha':alpha,'serial':serial,'city':city})
         if duplicate != None:
             return False
-        cl.insert_one({'creator':creator, 'type':type, 'selectDevice':selectDevice, 'idplate':idplate,'alpha':alpha,'serial':serial,'city':city, 'status':status,'pinout':pinout, 'duration':duration, "datetime":datetime.datetime.now()})
+        cl.insert_one({'creator':creator, 'idplate':idplate,'alpha':alpha,'serial':serial,'city':city, 'status':status, "datetime":datetime.datetime.now()})
         return True
     
     def get_all_rules(self):
